cddbc1526ea7f079b6140fd05e724e78f2c9e2d8
Clément Courageux-Sudan <clement.courageux-sudan@inria.fr>
2021-02-17 11:37:50 +0100

    script

---
+++ rabbitmqService/deployInfra.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# launch locally (1 node) services of bench infrastructures
+[ -z "${N1COST}" ] && N1COST="1000000"
+[ -z "${N1COST}" ] && N2COST="1000000"
+[ -z "${logDir}" ] && logDir="./logs/"
+
+if [[ ! -e ${logDir} ]]
+then
+    mkdir -p ${logDir}
+fi
+
+
+if [[ $1 = "1" ]]
+then
+    echo "Configuration 1: 1 service -> 1 sink (then plug-in the source to queue 'computeTask' start)"
+    python -u computeService.py -r localhost -i computeTask -o sink -n compute -c ${N1COST} 2>&1 | tee "${logDir}/1_S1.log" &
+    python -u sinkService.py -r localhost -i sink -n sink 2>&1 | tee > "${logDir}/1_Sink.log" &
+elif [[ $1 = "2" ]]
+then
+    echo "Configuration 2: 1 service -> 1 service -> 1 sink (then plug-in the source to queue 'computeTask' start)"
+    python -u computeService.py -r localhost -i computeTask -o computeTask2 -n compute -c ${N1COST} 2>&1 | tee "${logDir}/2_S1.log" &
+    python -u computeService.py -r localhost -i computeTask2 -o sink -n compute -c ${N2COST} 2>&1 | tee "${logDir}/2_S2.log"
+    python -u sinkService.py -r localhost -i sink -n sink 2>&1 | tee "${logDir}/2_Sink.log"
+elif [[ $1 = "kill" ]]
+then
+     echo "kill running services and sinks"
+     pkill -9 -f sinkService.py
+     pkill -9 -f computeService.py
+fi
+
+    
