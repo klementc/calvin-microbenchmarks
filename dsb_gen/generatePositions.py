5c1523709e2367aa0739fecb393f1cbbf3a59ea2
Clément Courageux-Sudan <clement.courageux-sudan@inria.fr>
2021-04-12 17:43:29 +0200

    fix

--- dsb_gen/generatePositions.py
+++ dsb_gen/generatePositions.py
@@ -65,13 +65,13 @@ class Drawing:
 
 def createPic(positions, fname):
     h = 500
-    graph = Drawing(len(positions)*(250+15),h)
+    graph = Drawing(len(hostnames)*(250+40),h)
     posTxt=[3]*len(positions)
 
     for i in range(len(hostnames)):
         graph.draw_rect(250,h,i*(250+35),0, col="rgb(174, 213, 129)")
         graph.draw_rect(240,35,i*(250+35),1, col="rgb(255, 245, 157)")
-        graph.draw_text("HOST: "+hostnames[i], i*250+35,25)
+        graph.draw_text("HOST: "+hostnames[i], i*(250+35),25)
         for j in p[i]:
             print("serv %s on node %s"%(j,hostnames[i]))
             graph.draw_text(j, 5+i*(250+35),posTxt[i]*25)
@@ -82,7 +82,7 @@ def createPic(positions, fname):
 
 
 
-positions = make_sets(servs, num_of_boxes=2)
+positions = make_sets(servs, num_of_boxes=len(hostnames))
 
 #for p in positions:
 #    for box in p:
@@ -95,7 +95,7 @@ for pi in range(len(positions)):
     p = positions[pi]
     awkPath=outDir+"/%d_awkfile.awk"%(pi)
     write_awk_for_config(p, hostnames, fname=awkPath)
-    os.system("awk -f %s %s  > %s/docker_compose_%d.yml"%(awkPath,templateFile,outDir,pi))
+    os.system("awk -f %s %s  > %s/%d_docker_compose.yml"%(awkPath,templateFile,outDir,pi))
     createPic(p, outDir+"/%d_deployment.svg"%(pi))
     for i in range(len(hostnames)):
         for j in p[i]:
